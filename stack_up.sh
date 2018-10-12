449082d4805944f6b9f7dbcdba9ff41079d6e005
Deshi Xiao <xiaods@gmail.com>
2018-10-12 13:15:36 +0800

    upgrade k8s multi instance case

--- stack_up.sh
+++ stack_up.sh
@@ -5,7 +5,7 @@ else
   echo "usage: stack_up.sh <namespace>"
   exit
 fi
-kubectl create -f zookeeper_micro.yaml "$NAMESPACE"
-while [ $(kubectl get po "$NAMESPACE" -o=wide | grep zk | grep Run | wc -l) -ne 1 ] ; do sleep 3; echo "Zookeper cluster not ready. Will try again after 3 sec..." ;done
+kubectl create -f zookeeper.yaml "$NAMESPACE"
+while [ $(kubectl get po "$NAMESPACE" -o=wide | grep zk | grep Run | wc -l) -ne 3 ] ; do sleep 3; echo "Zookeper cluster not ready. Will try again after 3 sec..." ;done
 kubectl create -f services "$NAMESPACE"
 kubectl create -f statefulsets "$NAMESPACE"
