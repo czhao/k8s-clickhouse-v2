59e3e9104e77f2c74fd265fa5dbba305382b8534
Deshi Xiao <xiaods@gmail.com>
2018-11-18 22:11:33 +0800

    update folder name

--- stack_down_micro.sh
+++ stack_down_micro.sh
@@ -7,5 +7,5 @@ else
 fi
 
 kubectl delete -f zookeeper_micro.yaml "$NAMESPACE"
-kubectl delete -f statefulsets "$NAMESPACE"
-kubectl delete -f services "$NAMESPACE"
+kubectl delete -f statefulsets_micro "$NAMESPACE"
+kubectl delete -f services_micro "$NAMESPACE"
--- stack_up_micro.sh
+++ stack_up_micro.sh
@@ -7,5 +7,5 @@ else
 fi
 kubectl create -f zookeeper_micro.yaml "$NAMESPACE"
 while [ $(kubectl get po "$NAMESPACE" -o=wide | grep zk | grep Run | wc -l) -ne 1 ] ; do sleep 3; echo "Zookeper cluster not ready. Will try again after 3 sec..." ;done
-kubectl create -f services "$NAMESPACE"
-kubectl create -f statefulsets "$NAMESPACE"
+kubectl create -f services_micro "$NAMESPACE"
+kubectl create -f statefulsets_micro "$NAMESPACE"
